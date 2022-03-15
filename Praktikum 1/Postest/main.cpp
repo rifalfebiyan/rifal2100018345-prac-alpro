2c0fab0c32599ec6f92c15d758c525b269cb7cfc
rifalfebiyan <rifalfebiyan123@gmail.com>
2022-03-15 14:42:01 +0700

    membuat main-cpp

---
+++ Praktikum 1/Postest/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(){
+	int a,b,hasil;
+	cout<<"Bilangann A: ";cin>>a;
+	cout<<"Bilangan B: ";cin>>b;
+	
+	hasil=a+b;
+	cout<<"Hasil penjumlahan kedua bilangan tersebut adalah: "<<hasil;
+}
