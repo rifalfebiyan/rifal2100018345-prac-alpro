1e00d3a4663b3fdb919214960e9dcb343275d4a7
rifalfebiyan <rifalfebiyan123@gmail.com>
2022-03-15 14:53:34 +0700

    update main-cpp baru

--- Praktikum 1/Postest/main.cpp
+++ Praktikum 1/Postest/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 main(){
-	int a,b,hasil;
+	int a,b,c,hasil;
 	cout<<"Bilangann A: ";cin>>a;
 	cout<<"Bilangan B: ";cin>>b;
+	cout<<"Bilangan C: ";cin>>c;
 	
-	hasil=a+b;
-	cout<<"Hasil penjumlahan kedua bilangan tersebut adalah: "<<hasil;
+	hasil=a+b+c;
+	cout<<"Hasil penjumlahan ketiga bilangan tersebut adalah: "<<hasil;
 }
