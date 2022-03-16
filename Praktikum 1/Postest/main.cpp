81719b90da8c5de82038a36431cbd669db7f2b0d
rifalfebiyan <rifalfebiyan123@gmail.com>
2022-03-16 20:32:04 +0700

    KERJA KELOMPOK

--- Praktikum 1/Postest/main.cpp
+++ Praktikum 1/Postest/main.cpp
@@ -1,11 +1,101 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-main(){
-	int a,b,c,hasil;
-	cout<<"Bilangann A: ";cin>>a;
-	cout<<"Bilangan B: ";cin>>b;
-	cout<<"Bilangan C: ";cin>>c;
-	
-	hasil=a+b+c;
-	cout<<"Hasil penjumlahan ketiga bilangan tersebut adalah: "<<hasil;
+ 
+void Jumlah(){
+    float a, b;
+    float hasil;
+    system("CLS");
+    cout << "Bilangan Pertama : "; cin >> a;
+    cout << "Bilangan Kedua   : "; cin >> b;
+    hasil = a+b;
+    cout << "\nHasil "<<a<<" + "<<b<<" adalah" << endl;
+    cout << "= "<<hasil;
+}
+ 
+void Kurang(){
+    float a, b;
+    float hasil;
+    system("CLS");
+    cout << "Bilangan Pertama : "; cin >> a;
+    cout << "Bilangan Kedua   : "; cin >> b;
+    hasil = a-b;
+    cout << "\nHasil "<<a<<" - "<<b<<" adalah" << endl;
+    cout << "= "<<hasil;
+}
+ 
+void Kali(){
+    float a, b;
+    float hasil;
+    system("CLS");
+    cout << "Bilangan Pertama : "; cin >> a;
+    cout << "Bilangan Kedua   : "; cin >> b;
+    hasil = a*b;
+    cout << "\nHasil "<<a<<" x "<<b<<" adalah"<<endl;
+	cout << "= "<<hasil;
+}
+ 
+void Bagi(){
+    float a, b;
+    float hasil;
+    system("CLS");
+    cout << "Bilangan Pertama : "; cin >> a;
+    cout << "Bilangan Kedua   : "; cin >> b;
+    hasil = a/b;
+    cout << "\nHasil "<<a<<" / "<<b<<" adalah" << endl;
+    cout << "= "<<hasil;
+}
+int main(){
+ 	int opr,answer;
+ 	char u;
+ 	do {
+ 		system("CLS");
+	 	cout << "PROGRAM KALKULATOR" << endl;
+	 	cout << "  1) PILIH MENU OPERASI  " << endl;
+	 	cout << "  2) KELUAR" << endl;
+	 	cout<<"Pilihan : ";cin >>answer;
+	 	
+ 		if(answer==1)
+ 		{
+	 		system("CLS");
+		    cout << "MENU OPERASI : " << endl;
+		    cout << "  1. Penjumlahan" << endl;
+		    cout << "  2. Pengurangan" << endl;
+		    cout << "  3. Perkalian" << endl;
+		    cout << "  4. Pembagian" << endl;
+		    cout << "Pilih Menu No \t : ";
+		    cin >> opr;
+		    
+			if (opr==1)
+			{
+				Jumlah();
+			}
+			else if(opr==2)
+			{
+				Kurang();
+			}
+			else if(opr==3)
+			{
+				Kali();
+			}
+			else if(opr==4)
+			{
+				Bagi();
+			}
+			cout<<endl;
+			cout<<endl;
+			system("pause");
+ 		}
+		else if(answer==2)
+		{
+			system("CLS");
+			cout<<"\nTerimakasih sudah menggunakan kalkulator ini";
+		}
+		else
+		{
+			cout<<"PILIH MENU DENGAN BENAR !!!!!";
+		}
+ 	}	
+ 	while (answer==1);
+ 	return 0; 
 }
